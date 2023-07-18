4637fd4403c29b57c6c63c9302350e058ffae6d9
nappelbaum <7777n@mail.ru>
2023-07-18 16:34:44 +0300

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import MyButton from "./UI/buttons/MyButton";
 const Header = ({ changeNavFix }) => {
   const [active, setActive] = useState(false);
   const { ref, inView, entry } = useInView({
-    threshold: 0,
+    threshold: 0.1,
   });
 
   useEffect(() => {
@@ -14,49 +14,50 @@ const Header = ({ changeNavFix }) => {
     changeNavFix(active);
   }, [entry, inView, active, changeNavFix]);
 
-  const btnScroll = useRef(null);
+  const divScroll = useRef(null);
   const onButtonClick = () => {
-    const btnScrollCoords = btnScroll.current.getBoundingClientRect();
+    console.log(window.outerHeight);
+    const divScrollCoords = divScroll.current.getBoundingClientRect();
     window.scrollTo({
       top:
-        btnScrollCoords.top + window.pageYOffset + btnScrollCoords.height + 80,
+        divScrollCoords.top +
+        window.pageYOffset +
+        divScrollCoords.height * (window.outerHeight > 630 ? 0.94 : 1.05),
       behavior: "smooth",
     });
   };
 
   return (
-    <header ref={ref} className={`header${!active ? "" : " header--margin"}`}>
-      <div className="container">
-        <div className="header__row">
-          <div className="header__text">
-            <div className="category">your snowboard guide</div>
-            <h1 className="header__title">
-              Привет!
-              <br />
-              Я ваш персональный <br />
-              инструктор по сноуборду))
-            </h1>
-            <MyButton
-              className="button header__btn"
-              ref={btnScroll}
-              onClick={onButtonClick}
-            >
-              <span>Погнали</span>
+    <div ref={divScroll}>
+      <header ref={ref} className="header">
+        <div className="container">
+          <div className="header__row">
+            <div className="header__text">
+              <div className="category">your snowboard guide</div>
+              <h1 className="header__title">
+                Привет!
+                <br />
+                Я ваш персональный <br />
+                инструктор по сноуборду))
+              </h1>
+              <MyButton className="button header__btn" onClick={onButtonClick}>
+                <span>Погнали</span>
+                <img
+                  src={require("./../img/svg/curve-down-arrow.png")}
+                  alt="Погнали"
+                />
+              </MyButton>
+            </div>
+            <div className="header__img">
               <img
-                src={require("./../img/svg/curve-down-arrow.png")}
-                alt="Погнали"
+                src={require("./../img/main1.png")}
+                alt="инструктор по сноуборду"
               />
-            </MyButton>
-          </div>
-          <div className="header__img">
-            <img
-              src={require("./../img/main1.png")}
-              alt="инструктор по сноуборду"
-            />
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
